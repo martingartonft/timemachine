e717f1c69daa6853f449e74d6ba9c064a6762094
Martin Garton <martin.garton@ft.com>
2015-04-20 17:55:19 +0100

    tidy up and skip instead of crashing when transform fails.

--- importer/import.go
+++ importer/import.go
@@ -7,24 +7,12 @@ import (
 
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
 
 type importerContent struct {
-	/*
-		//ID            string   `json:"id"`
-		UUID          string    `json:"uuid"`
-		URI           string    `json:"id"`
-		Body       string    `json:"body,omitempty"`
-		Brands        map[string]interface{} `json:"brands,omitempty"`
-		Byline        string    `json:"byline,omitempty"`
-		PublishedDate time.Time `json:"publishedDate,omitempty"`
-		//RequestUrl    string   `json:"requestUrl"`
-		Title string `json:"title,omitempty"`
-		//Type   string `json:"type"`
-		WebUrl string `json:"webUrl,omitempty"`
-	*/
 	Body   string `json:"body"`
 	Brands []struct {
 		ID string `json:"id"`
@@ -37,10 +25,6 @@ type importerContent struct {
 	} `json:"identifiers"`
 	InternalBinaryUrl interface{} `json:"internalBinaryUrl"`
 	MainImage         string      `json:"mainImage"`
-	MediaType         interface{} `json:"mediaType"`
-	Members           interface{} `json:"members"`
-	PixelHeight       interface{} `json:"pixelHeight"`
-	PixelWidth        interface{} `json:"pixelWidth"`
 	PublishedDate     time.Time   `json:"publishedDate"`
 	Title             string      `json:"title"`
 	Titles            interface{} `json:"titles"`
@@ -49,7 +33,24 @@ type importerContent struct {
 
 func main() {
 	//	uuids := []string{"16551bd2-5960-4cf3-a8fa-db4707df470c"}
-	uuids := []string{"b808e3a2-e740-11e4-8e3f-00144feab7de"}
+	uuids := []string{
+		"b808e3a2-e740-11e4-8e3f-00144feab7de",
+		"2e61fc44-e761-11e4-a01c-00144feab7de",
+		"d8cf7278-e75d-11e4-a01c-00144feab7de",
+		"e258a2b0-e76c-11e4-a01c-00144feab7de",
+		"66b43544-e739-11e4-a01c-00144feab7de",
+		"1f74fac0-e74c-11e4-8e3f-00144feab7de",
+		"533012c6-e74e-11e4-8e3f-00144feab7de",
+		"77b19124-e76c-11e4-8e3f-00144feab7de",
+		"563485b4-e745-11e4-8e3f-00144feab7de",
+		"b0414178-e73b-11e4-a01c-00144feab7de",
+		"056f148a-e751-11e4-a01c-00144feab7de",
+		"96544f60-e3f0-11e4-9a82-00144feab7de",
+		"80eeb418-e700-11e4-9fa1-00144feab7de",
+		"9fe1a040-debf-11e4-8a01-00144feab7de",
+		"946a1d42-e1f2-11e4-9995-00144feab7de",
+		"1c1ed6a8-e75f-11e4-a01c-00144feab7de",
+	}
 
 	eomFileJsons := make(chan string, 16)
 	contentJsons := make(chan []byte, 16)
@@ -129,8 +130,8 @@ func queueEomJsons(eomJsons chan<- string, uuids []string) {
 			}
 			eomJsons <- string(eomJson)
 		}
-		close(eomJsons)
 	}
+	close(eomJsons)
 }
 
 func queueContentJsons(eomFileJsons <-chan string, contentJsons chan []byte) {
@@ -139,12 +140,14 @@ func queueContentJsons(eomFileJsons <-chan string, contentJsons chan []byte) {
 		if err != nil {
 			panic(err)
 		}
-		if resp.StatusCode != 200 {
-			panic(fmt.Sprintf("transform failed with code %d\n", resp.StatusCode))
-		}
 		response, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			panic("reading response body failed")
+		}
+		if resp.StatusCode != 200 {
+
+			log.Printf("transform failed for %v with code %d, skipping:\n%v\n", eomJson, resp.StatusCode, string(response))
+			continue
 		}
 		contentJsons <- response
 		resp.Body.Close()
