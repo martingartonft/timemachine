18b4286d336c76b0180b20799047a59368f6baf0
Martin Garton <martin.garton@ft.com>
2015-04-20 11:41:16 +0100

    make domain more like CAPI one

--- api/domain.go
+++ api/domain.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+/*
 type Content struct {
 	UUID            string    `json:"uuid"`
 	LastPublishDate time.Time `json:"last-published-time"`
@@ -11,4 +12,17 @@ type Content struct {
 	TextBody        string    `json:"bodyPlain"`
 	Link            string    `json:"link,omitempty"`
 	Genre           string    `json:"genre,omitempty"`
+}
+*/
+type Content struct {
+	//ID            string   `json:"id"`
+	UUID          string    `json:"uuid"`
+	BodyXML       string    `json:"bodyXML"`
+	Brands        []string  `json:"brands"`
+	Byline        string    `json:"byline"`
+	PublishedDate time.Time `json:"publishedDate"`
+	//RequestUrl    string   `json:"requestUrl"`
+	Title  string `json:"title"`
+	Type   string `json:"type"`
+	WebUrl string `json:"webUrl"`
 }
